f882a008f523d243acb358fffbb58279690e71e4
Joaquin Rodriguez <joaquinrodriguez1991@gmail.com>
2021-02-08 10:50:28 +0000

    Button example added

---
+++ PyQtExamples/ButtonsExample/README.md
@@ -0,0 +1,7 @@
+# Button example
+This example adds a button to the MainWindow. This button is connected with a
+method, in such a way that each time we push it, the function is called. In this
+example, each time we click the button, it prints a message in the console.
+
+# Application screenshot
+![app screenshot](/PyQtExamples/ButtonsExample/images/ButtonExample.png)
---
+++ PyQtExamples/ButtonsExample/mainwindow.py
@@ -0,0 +1,92 @@
+from PyQt5.QtWidgets import \
+    QScrollArea, \
+    QHBoxLayout, \
+    QMainWindow, \
+    QPushButton, \
+    QVBoxLayout, \
+    QWidget
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        '''
+        Constructor. In this function, we create all the variables we will use
+        in the code. If we cannot assign any value yet, we assign None. This way,
+        from the beginning of the class, we know all the variables that belongs
+        to the class. If not, we have to read all the code in order to know, and
+        if we read the variable and it has not been created yet, the program will
+        crash.        
+        '''
+        super(MainWindow, self).__init__(parent)
+
+        self.right_layout = QVBoxLayout()
+        self.left_layout = QVBoxLayout()
+        self.main_layout = QHBoxLayout()
+
+        self.initialize_widget()
+        self.setWindowTitle("This is my buttons example")
+
+    def initialize_widget(self):
+        '''
+        Function that initializes the MainWindow. It will add all the widgets
+        attached to it. Each widget might have some initializations to do also,
+        so those initializations must be done before exiting this function.
+
+        The recommended way to do it is that each widget to attach will be added
+        by calling a method that will do all the required initialization for that
+        widget.
+        '''
+        self.add_button()
+
+        # Add the widgets that we want to add to the MainWindow must be added
+        # before this line
+        self.set_main_window_layouts()
+
+    def set_main_window_layouts(self):
+        '''
+        Set the MainWindow Layout. We change the default MainWindow widget
+        to be in two parts: Left and Right. So we can, for instance, show
+        images in the left, and have control buttons to the right.
+
+        For that, two layouts exists in the program: self.left_layout and
+        self.right_layout.
+        '''
+        # We create scroll area to allows to slide the window, when the screen
+        # size is not enough
+        main_scroll = QScrollArea()
+
+        # The MainWindow is splitted into two areas, left and right, each of them
+        # with the corresponding layouts
+        left_widget = QWidget()
+        right_widget = QWidget()
+        main_widget = QWidget()
+
+        left_widget.setLayout(self.left_layout)
+        right_widget.setLayout(self.right_layout)
+
+        self.main_layout.addWidget(left_widget)
+        self.main_layout.addWidget(right_widget)
+        main_widget.setLayout(self.main_layout)
+
+        main_scroll.setWidget(main_widget)
+        main_scroll.setWidgetResizable(True)
+
+        self.setCentralWidget(main_scroll)
+
+    def add_button(self):
+        '''
+        We add a button to the main window.
+        '''
+        ## We add a button with the legend 'This is my button' on it.
+        myButton = QPushButton("This is my button", self)
+        # We make a connection between the signal executed whenever the button is pushed
+        # and the function self.on_button_clicked. This way, each time we
+        # push the button, on_button_clicked is called
+        myButton.clicked.connect(self.on_button_clicked)
+        ## Once we have our widget, we add it to the layout, so it will be placed
+        # in the right place. Without this line, all the widgets will be placed one
+        # on top of the other one, in the top left corner of the window
+        self.right_layout.addWidget(myButton)
+
+    def on_button_clicked(self):
+        # Here you place your actions to execute when you push the button
+        print("Hey! You pushed me!!.... THANK YOUUU!!")
