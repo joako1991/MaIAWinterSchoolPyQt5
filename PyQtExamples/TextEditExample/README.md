fe29f0c8916d159b91cf519a727f5e723c84a5e3
Joaquin Rodriguez <joaquinrodriguez1991@gmail.com>
2021-02-11 10:30:20 +0000

    Text Edit example added

---
+++ PyQtExamples/TextEditExample/README.md
@@ -0,0 +1,20 @@
+# Text edit example
+The Text edit object from Qt allows to create text in several lines. It allows
+also to change text parameters, like type of letter, size, and alignment, between
+others. Then, entered text, can be retrieved, and for instance, be stored in
+a text file.
+
+Another possible application is, if we are willing users to write comments
+about the application, we can use this object to allow them to enter a bigger
+text, than a simple Line Edit. We can also write chat applications with it,
+where we show the text entered by each participant of the chat on the text edit
+object.
+
+This object can be also set as read-only, to avoid the users to enter text directly
+on it, but we can add lines into it using lines of code instead.
+
+In this example, we show how to create the QTextEdit object, how to add text
+by code, change the mode to read-only, and add it to the layout.
+
+# Application screenshot
+![app screenshot](/PyQtExamples/TextEditExample/images/TextEditExample.png)
---
+++ PyQtExamples/TextEditExample/mainwindow.py
@@ -0,0 +1,112 @@
+from PyQt5.QtWidgets import \
+    QHBoxLayout, \
+    QMainWindow, \
+    QTextEdit, \
+    QScrollArea, \
+    QVBoxLayout, \
+    QWidget
+
+from PyQt5.QtCore import \
+    Qt
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        '''
+        Constructor. In this function, we create all the variables we will use
+        in the code. If we cannot assign any value yet, we assign None. This way,
+        from the beginning of the class, we know all the variables that belongs
+        to the class. If not, we have to read all the code in order to know, and
+        if we read the variable and it has not been created yet, the program will
+        crash.
+        '''
+        super(MainWindow, self).__init__(parent)
+
+        self.right_layout = QVBoxLayout()
+        self.left_layout = QVBoxLayout()
+        self.main_layout = QHBoxLayout()
+
+        self.plain_text_box = None
+
+        self.initialize_widget()
+        self.setWindowTitle("This is my template example")
+
+    def initialize_widget(self):
+        '''
+        Function that initializes the MainWindow. It will add all the widgets
+        attached to it. Each widget might have some initializations to do also,
+        so those initializations must be done before exiting this function.
+
+        The recommended way to do it is that each widget to attach will be added
+        by calling a method that will do all the required initialization for that
+        widget.
+        '''
+        ## Here you start initializing your widgets
+        self.add_plain_text_edit()
+
+        # Add the widgets that we want to add to the MainWindow must be added
+        # before this line
+        self.set_main_window_layouts()
+
+    def set_main_window_layouts(self):
+        '''
+        Set the MainWindow Layout. We change the default MainWindow widget
+        to be in two parts: Left and Right. So we can, for instance, show
+        images in the left, and have control buttons to the right.
+
+        For that, two layouts exists in the program: self.left_layout and
+        self.right_layout.
+        '''
+        # We create scroll area to allows to slide the window, when the screen
+        # size is not enough
+        main_scroll = QScrollArea()
+
+        # The MainWindow is splitted into two areas, left and right, each of them
+        # with the corresponding layouts
+        left_widget = QWidget()
+        right_widget = QWidget()
+        main_widget = QWidget()
+
+        left_widget.setLayout(self.left_layout)
+        self.right_layout.addStretch(1)
+        right_widget.setLayout(self.right_layout)
+
+        self.main_layout.addWidget(left_widget)
+        self.main_layout.addWidget(right_widget)
+        main_widget.setLayout(self.main_layout)
+
+        main_scroll.setWidget(main_widget)
+        main_scroll.setWidgetResizable(True)
+
+        self.setCentralWidget(main_scroll)
+
+    def add_plain_text_edit(self):
+        '''
+        This function adds a Text edit box. This object allows to add several
+        lines of text, that we can retrieve at the end. In this case, we set
+        it as read-only, which means we cannot write by the user interface, but
+        we can add text using lines of code. We can modify several of its
+        parameters in order to make a better text: Alignment, italic, bold,
+        letter size, letter type, etc, as in any text editor like Word from Microsoft or
+        Writer from OpenOffice.
+        '''
+        # We create our QTextEdit object
+        self.plain_text_box = QTextEdit(self)
+        # We change it to be read-only, because we do not want the user to
+        # write text
+        self.plain_text_box.setReadOnly(True)
+
+        # We add a line at the bottom of all the text
+        self.plain_text_box.append("Hello")
+
+        # We add another line at the bottom of all the text
+        self.plain_text_box.append("My")
+        # We change the alignment of the last inserted text
+        self.plain_text_box.setAlignment(Qt.AlignCenter)
+
+        # We add another line at the bottom of all the text
+        self.plain_text_box.append("World")
+        # We change the alignment of the last inserted text
+        self.plain_text_box.setAlignment(Qt.AlignRight)
+
+        # We add our widget to the left layout
+        self.left_layout.addWidget(self.plain_text_box)
